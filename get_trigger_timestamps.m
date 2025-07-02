f8958952786b5f3575a795748dce97a1b6f209f2
Josephine Timm <117468201+0815Phine@users.noreply.github.com>
2025-07-02 17:15:24 +0200

    Update get_trigger_timestamps.m

--- get_trigger_timestamps.m
+++ get_trigger_timestamps.m
@@ -1,9 +1,9 @@
 % 
-dir = 'Z:/Animals/Test_files/';
+dir = 'Z:/Animals/Test_files/250702';
 
-%%
+%% from wavesurfer Pulse Train
 % read wavesurfer file
-filename = '#Test_cam_5_0001.h5';
+filename = 'hab_test_2_0001.h5';
 
 data = ws.loadDataFile(fullfile(dir,filename)); %using wavesurfer function
 
@@ -14,21 +14,26 @@ Fs = data.header.AcquisitionSampleRate;
 %numSamples = size(data.sweep_0001.analogScans, 1);
 %timeVector = (0:numSamples-1) / Fs;
 
-channelIndex = 1; %Pulse Train
-pulse_signal = data.sweep_0001.analogScans(:, channelIndex);
-numSamples = length(pulse_signal);
+chanFlag = find(contains(data.header.AIChannelNames, 'Cam'));
+for i = 1:length(chanFlag)
+    chanIdx = chanFlag(i);
+    pulse_signal = data.sweep_0001.analogScans(:, chanIdx);
+    %numSamples(chanIdx) = length(pulse_signal);
 
-% get trigger timestamps
-threshold = mean(pulse_signal);
-risingEdges = find(diff(pulse_signal > threshold) == 1); % find rising edges
-t_ts = (risingEdges/Fs)/60; %in minutes
+    % get trigger timestamps
+    threshold = mean(pulse_signal);
+    risingEdges = find(diff(pulse_signal > threshold) == 1); % find rising edges
+    t_ts{i} = (risingEdges/Fs)/60; %in minutes
+    clear("pulse_signal")
+end
 
-%%
-videoname = '#Test_cam_5.avi';
+%% from video file (this takes long!!!)
+videoname = '#Test_hab_2.mp4';
 vidObj = VideoReader(fullfile(dir,videoname));
 numFrames = vidObj.NumFrames;
 
+%% from timestamps.txt file
 % load timestamps
-v_ts = load(fullfile(dir,'#Test_cam_5_video_timestamps.txt'));
+v_ts = load(fullfile(dir,'#Test_hab_2_video_timestamps.txt'));
 time_diff = diff(v_ts);
 cumulative_time = ([0; cumsum(time_diff)])/60;
